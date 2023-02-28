d5c92ea444884b8bcb03b8ebf93422f418ea1ba1
Anatolii Zorin <Zorger27@GMail.com>
2023-02-28 05:46:50 +0200

    Small changes.

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,8 +1,14 @@
+<script lang="ts">
+import {Vue} from "vue-class-component";
+export default class experience extends Vue {};
+</script>
+
 <template>
   <div class="experience">
     <h1>Experience</h1>
     <line></line>
-      <p>15.01.2001 - 13.01.2023<br>
+    <codersrank-summary username="zorger27" layout="horizontal" branding="false"></codersrank-summary>
+    <p>15.01.2001 - 13.01.2023<br>
         <b>LIS Ltd.</b><br>
         CEO<br>
         <i>Full enterprise management.</i><br>
@@ -15,17 +21,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import {Vue} from "vue-class-component";
-export default class experience extends Vue {}
-</script>
-
 <style lang="scss" scoped>
 .experience {
   flex: 1 0 auto;
   text-align: left;
   p {
     padding: 0 0 0.3rem 1.5rem;
+  }
+  codersrank-summary {
+    margin-top: 1rem;
+    width: 75%;
+  }
+  @media(max-width:1200px) {
+    codersrank-summary {
+      width: 100%;
+    }
+  }
+  @media(max-width:768px) {
+    codersrank-summary {
+      width: 100%;
+    }
   }
 }
 </style>
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -2,23 +2,28 @@
   <div class="skills">
     <h1>Hard & Soft skills</h1>
     <line></line>
-    <div class="hard-skills">
-      <h3>Hard skills</h3>
-      <ul>
-        <li><span>&#9745;</span>Html5, CSS3, JavaScript</li>
-        <li><span>&#9745;</span>Vue.JS, Angular</li>
-        <li><span>&#9745;</span>Git, Github</li>
-        <li><span>&#9745;</span>Figma, Photoshop</li>
-      </ul>
-    </div>
-    <div class="soft-skills">
-      <h3>Soft skills</h3>
-      <ul>
-        <li><span>&#9745;</span>Management</li>
-        <li><span>&#9745;</span>Strategic Thinking</li>
-        <li><span>&#9745;</span>Critical Thinking</li>
-        <li><span>&#9745;</span>Negotiation</li>
-      </ul>
+    <codersrank-skills-chart username="zorger27" labels="true" legend="true" active-skills="JSON, JavaScript, Vue, CSS, SCSS, HTML, TypeScript" branding="false"></codersrank-skills-chart>
+    <div class="container">
+      <div class="type-skills">
+        <div class="hard-skills">
+          <h3>Hard skills</h3>
+          <ul>
+            <li><span>&#9745;</span>Html5, CSS3, JavaScript</li>
+            <li><span>&#9745;</span>Vue.JS, Angular</li>
+            <li><span>&#9745;</span>Git, Github</li>
+            <li><span>&#9745;</span>Figma, Photoshop</li>
+          </ul>
+        </div>
+        <div class="soft-skills">
+          <h3>Soft skills</h3>
+          <ul>
+            <li><span>&#9745;</span>Management</li>
+            <li><span>&#9745;</span>Strategic Thinking</li>
+            <li><span>&#9745;</span>Critical Thinking</li>
+            <li><span>&#9745;</span>Negotiation</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +45,14 @@ export default class skills extends Vue {}
   span {
     margin-right: 0.5rem;
   }
+  .container {
+    display: flex;
+    justify-content: left;
+  }
+  .type-skills {
+    display: inline-flex;
+    margin-right: 0.5rem;
+  }
   .hard-skills ul {
     list-style: none;
     padding: 0 0 0.3rem 1.5rem;
@@ -47,5 +60,21 @@ export default class skills extends Vue {}
   .soft-skills ul {
     list-style: none;
     padding: 0 0 0.3rem 1.5rem;  }
+
+  codersrank-skills-chart {
+    margin-top: 1rem;
+    padding-right: 0.5rem;
+    width: 75%;
+  }
+  @media(max-width:1200px) {
+    codersrank-skills-chart {
+      width: 100%;
+    }
+  }
+  @media(max-width:768px) {
+    codersrank-skills-chart {
+      width: 100%;
+    }
+  }
 }
 </style>
