48322624d1de6dd23e2e2b7d7f1a13bad00d20b8
Anatolii Zorin <Zorger27@GMail.com>
2023-03-07 04:37:15 +0200

    Small changes.

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -21,7 +21,9 @@ export default class projects extends Vue {}
         <h3>{{ prj.id }}. {{ prj.title }}</h3>
         <div>Type: <strong>{{ prj.type }}</strong></div>
         <div>Technologies: <strong>{{ prj.techno }}</strong></div>
-        <a v-if="prj.demolink" :href="prj.demolink" title="Link to the project" target="_blank"><b>Link to the project</b></a>
+        <div v-if="prj.demolink" class="demolink" >
+          <a :href="prj.demolink" title="Link to the project" target="_blank"><b>Link to the project</b></a>
+        </div>
       </a>
     </div>
   </div>
@@ -32,16 +34,32 @@ export default class projects extends Vue {}
   flex: 1 0 auto;
   .prj {
     .block {
-     a {
+      .demolink {
         text-decoration: none;
-        color: darkred;
+        color: white;
+        display: inline-flex;
+        border: 1px solid darkred;
+        padding: 0.5rem;
+        margin: 0.3rem 0;
+        border-radius: 5px;
+        background-color:hsla(0, 100%, 50%, 1);
+        background-image:
+            radial-gradient(at 40% 20%, hsla(28,100%, 74%,1) 0px, transparent 50%),
+            radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%),
+            radial-gradient(at 0% 50%, hsla(355,100%, 93%, 1) 0px, transparent 50%),
+            radial-gradient(at 76% 52%, hsla(340,100%,76%, 1) 0px, transparent 50%),
+            radial-gradient(at 0% 100%, hsla(22,100%, 77%, 1) 0px, transparent 50%),
+            radial-gradient(at 79% 100%, hsla(242, 100%, 70%, 1) 0px, transparent 50%),
+            radial-gradient(at 0% 0%, hsla(343,100%, 76%, 1) 0px, transparent 50%);
+        a:hover {
+          border: none;
+          box-shadow: none;
+        }
       }
-      a:hover {
-        border: none;
-        box-shadow: none;
-        text-decoration: underline;
-        //font-weight: bold;
-        color: red;
+      .demolink:hover {
+        background: linear-gradient(to bottom, rgba(228, 229, 229, 0.5), rgba(72, 72, 171, 0.5));
+        color: darkblue;
+        border-color: darkslateblue;
       }
     }
   }
