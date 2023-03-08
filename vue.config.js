ae85910b80fd08a7716ed996dd1f8b3975b010f6
Anatolii Zorin <Zorger27@GMail.com>
2023-03-09 01:15:50 +0200

    Small changes.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = defineConfig({
     transpileDependencies: true,
     configureWebpack: {
         performance: {
             maxAssetSize: 5000000
-        }
-    },
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'my-index.html',
+                template: 'public/index.html',
+                title: 'My Curriculum VITAE (SPA)'
+            }),
+        ]
+    }
 })
