82999c8f868d015f4b5b8e9549121b85521fa2d8
Anatolii Zorin <Zorger27@GMail.com>
2023-03-10 06:18:42 +0200

    Small changes.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = defineConfig({
@@ -14,8 +15,22 @@ module.exports = defineConfig({
     },
     configureWebpack: {
         plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: `./public/assets`,
+                        to: 'assets'
+                    }
+                ]
+            }),
+            // new HtmlWebpackPlugin({
+            //     title: 'Webpack',
+            //     // favicon: './public/favicon.png',
+            //     template: '/public/index.html',
+            //     filename: 'index.html'
+            // }),
             new FaviconsWebpackPlugin({
-                logo: './src/assets/favicon/android-chrome-512x512.png',
+                logo: './src/assets/favicon/favbig.png',
                 favicons: {
                     appName: 'CV on Vue',
                     appDescription: 'My Curriculum VITAE (SPA) on Vue.JS',
