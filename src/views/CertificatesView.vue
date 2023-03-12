c0dc09b8e6e776131a1d3b93d5ce3c1cd59103a4
Anatolii Zorin <Zorger27@GMail.com>
2023-03-12 04:08:21 +0200

    Small changes.

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -17,11 +17,11 @@
         4) React Developer<br>
         5) Frontend Developer</i>
       </router-link></div>
-      <div class="prj"><b><router-link class="block" to="/certificates/database">DataBase Developer Certificates</router-link></b></div>
-      <div class="prj"><b><router-link class="block" to="/certificates/designer">UX/UI Designer Certificates</router-link></b></div>
-      <div class="prj"><b><router-link class="block" to="/certificates/pm">Project Management Certificates</router-link></b></div>
-      <div class="prj"><b><router-link class="block" to="/certificates/other">Other Certificates</router-link></b></div>
-      <div class="prj eng"><b><router-link class="block" to="/certificates/english">English language Certificates</router-link></b></div>
+      <div class="prj"><router-link class="block" to="/certificates/database"><b>DataBase Developer Certificates</b></router-link></div>
+      <div class="prj"><router-link class="block" to="/certificates/designer"><b>UX/UI Designer Certificates</b></router-link></div>
+      <div class="prj"><router-link class="block" to="/certificates/pm"><b>Project Management Certificates</b></router-link></div>
+      <div class="prj"><router-link class="block" to="/certificates/other"><b>Other Certificates</b></router-link></div>
+      <div class="prj eng"><router-link class="block" to="/certificates/english"><b>English language Certificates</b></router-link></div>
   </div>
 </template>
 
@@ -34,27 +34,9 @@ export default class certificates extends Vue {}
 <style lang="scss" scoped>
 .certificates {
   flex: 1 0 auto;
-   .prj {
-      display: flex;
-      justify-content: center;
-      font-size: 1.5rem;
-      margin-bottom: 1rem;
-      b {
-        color: darkred;
-      }
-    }
-  .prj:hover {
-    .block {
-      background: lightblue;
-      box-shadow: 3px 3px 4px 0 lightgrey;
-      border-color: lightskyblue;
-    }
-      b {
-        color: red;
-        text-decoration: none;
-      }
-    }
-
+  .block {
+    font-size: 1.2rem;
+  }
   .front, .back {
       b {
         text-decoration: underline;
@@ -62,15 +44,32 @@ export default class certificates extends Vue {}
     }
   .eng {
     b {
-      color: darkgreen;
-    }
-    b:hover{
       color: green;
     }
   }
-  .block {
-      font-size: 1.2rem;
+  //  b:hover{
+  //    color: green;
+  //  }
+  //}
+}
+.prj {
+  display: flex;
+  justify-content: center;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  b {
+    color: darkred;
   }
-
+}
+.prj:hover {
+  .block {
+    background: lightblue;
+    box-shadow: 3px 3px 4px 0 lightgrey;
+    border-color: lightskyblue;
+  }
+  b {
+    color: red;
+    text-decoration: none;
+  }
 }
 </style>
