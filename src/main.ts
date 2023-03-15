27a5ff7afc66c69dd013d5e91f6995c7f1b3f5e3
Anatolii Zorin <Zorger27@GMail.com>
2023-03-16 00:46:25 +0200

    Small changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// import VueGtag from "vue-gtag-next";
-// createApp(App).use(VueGtag, {property: {id: "G-RZXQRX8XR0"}}).use(store).use(router).mount('#app')
-
 createApp(App).use(store).use(router).mount('#app')
