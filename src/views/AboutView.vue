1a0cf51dd45bed66768bec452ba5b85678acecde
Anatolii Zorin <Zorger27@GMail.com>
2023-03-23 04:09:17 +0200

    Internationalization

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,15 +4,15 @@
         <line></line>
     <h2>{{$t ('about.technologies')}}</h2>
     <ol>
-      <li><a href="https://v3.ru.vuejs.org" title="Vue.JS v.3.0" target="_blank">Vue.JS v.3.0</a></li>
-      <li><a href="https://vuex.vuejs.org" title="Vuex" target="_blank">Vuex</a></li>
-      <li><a href="https://vue-i18n.intlify.dev" title="i18n" target="_blank">i18n</a></li>
+      <li><a href="https://v3.ru.vuejs.org" title="Vue.JS" target="_blank">Vue.JS v.3.2.13</a></li>
+      <li><a href="https://vuex.vuejs.org" title="Vuex" target="_blank">Vuex v.4.0.0</a></li>
+      <li><a href="https://vue-i18n.intlify.dev" title="i18n" target="_blank">i18n v.9.2.2</a></li>
       <li>HTML5</li>
       <li>CSS3</li>
       <li><a href="https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox" title="FlexBox CSS" target="_blank">FlexBox CSS</a></li>
       <li><a href="https://developer.mozilla.org/ru/docs/Web/CSS/CSS_Grid_Layout" title="Grid CSS" target="_blank">Grid CSS</a></li>
       <li>JavaScript</li>
-      <li>TypeScript</li>
+      <li>TypeScript v.4.5.5</li>
     </ol>
 <!--    <line></line>-->
 <!--    <h3>Project setup: <strong>npm install</strong></h3>-->
