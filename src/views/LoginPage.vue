388fa63a5840be202b38f78e413e89aea2f5225a
Anatolii Zorin <Zorger27@GMail.com>
2023-03-25 03:23:44 +0200

    Creating a Login Page.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -9,8 +9,13 @@ export default class LoginPage extends Vue {
 <template>
   <h1> Login Page </h1>
   <line></line>
+  <div class="login">
+    <h2> Here will be the authorization form! 😉 </h2>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.login {
+  flex: 1 0 auto;
+}
 </style>
