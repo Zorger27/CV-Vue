3327e3ada26f62df981ffb0dbeccf8866ce10c1e
Anatolii Zorin <Zorger27@GMail.com>
2023-03-27 02:50:55 +0300

    Small changes

--- src/views/certificates/CertificatesView.vue
+++ src/views/certificates/CertificatesView.vue
@@ -2,39 +2,41 @@
   <div class="certificates">
     <h1>{{ $t('cert.title') }}</h1>
     <line></line>
-    <div class="prj back">
-      <router-link class="block" to="/certificates/backend">
-        <b>{{ $t('cert.back.backend') }}</b><br>
-        <i>{{ $t('cert.back.java') }}<br>
-          {{ $t('cert.back.php') }}<br>
-          {{ $t('cert.back.python') }}<br>
-          {{ $t('cert.back.ruby') }}</i>
-      </router-link>
-    </div>
-    <div class="prj front">
-      <router-link class="block" to="/certificates/frontend">
-        <b>{{ $t('cert.front.frontend') }}</b><br>
-        <i>{{ $t('cert.front.html') }}<br>
-          {{ $t('cert.front.javascript') }}<br>
-          {{ $t('cert.front.angular') }}<br>
-          {{ $t('cert.front.react') }}<br>
-          {{ $t('cert.front.frontend2') }}</i>
-      </router-link>
-    </div>
-    <div class="prj">
-      <router-link class="block" to="/certificates/database"><b>{{ $t('cert.database') }}</b></router-link>
-    </div>
-    <div class="prj">
-      <router-link class="block" to="/certificates/designer"><b>{{ $t('cert.designer') }}</b></router-link>
-    </div>
-    <div class="prj">
-      <router-link class="block" to="/certificates/pm"><b>{{ $t('cert.project') }}</b></router-link>
-    </div>
-    <div class="prj">
-      <router-link class="block" to="/certificates/other"><b>{{ $t('cert.other') }}</b></router-link>
-    </div>
-    <div class="prj eng">
-      <router-link class="block" to="/certificates/english"><b>{{ $t('cert.english') }}</b></router-link>
+    <div class="container">
+      <div class="prj back">
+        <router-link class="block" to="/certificates/backend">
+          <b>{{ $t('cert.back.backend') }}</b><br>
+          <i>{{ $t('cert.back.java') }}<br>
+            {{ $t('cert.back.php') }}<br>
+            {{ $t('cert.back.python') }}<br>
+            {{ $t('cert.back.ruby') }}</i>
+        </router-link>
+      </div>
+      <div class="prj front">
+        <router-link class="block" to="/certificates/frontend">
+          <b>{{ $t('cert.front.frontend') }}</b><br>
+          <i>{{ $t('cert.front.html') }}<br>
+            {{ $t('cert.front.javascript') }}<br>
+            {{ $t('cert.front.angular') }}<br>
+            {{ $t('cert.front.react') }}<br>
+            {{ $t('cert.front.frontend2') }}</i>
+        </router-link>
+      </div>
+      <div class="prj database">
+        <router-link class="block" to="/certificates/database"><b>{{ $t('cert.database') }}</b></router-link>
+      </div>
+      <div class="prj designer">
+        <router-link class="block" to="/certificates/designer"><b>{{ $t('cert.designer') }}</b></router-link>
+      </div>
+      <div class="prj pm">
+        <router-link class="block" to="/certificates/pm"><b>{{ $t('cert.project') }}</b></router-link>
+      </div>
+      <div class="prj other">
+        <router-link class="block" to="/certificates/other"><b>{{ $t('cert.other') }}</b></router-link>
+      </div>
+      <div class="prj eng">
+        <router-link class="block" to="/certificates/english"><b>{{ $t('cert.english') }}</b></router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -50,41 +52,104 @@ export default class certificates extends Vue {
 <style lang="scss" scoped>
 .certificates {
   flex: 1 0 auto;
-  .block {
-    font-size: 1.2rem;
-  }
-  .front, .back {
-    b {
-      text-decoration: underline;
+  .container {
+    display: inline-grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+    //grid-template-rows: auto;
+    grid-template-areas:
+      "back front"
+      "database designer"
+      "pm other"
+      "eng eng";
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0;
+    grid-auto-flow: column;
+
+    .back {grid-area: back;}
+    .front {grid-area: front;}
+    .database {grid-area: database;}
+    .designer {grid-area: designer;}
+    .pm {grid-area: pm;}
+    .other {grid-area: other;}
+
+    .eng {
+      grid-area: eng;
+      //b {
+      //  color: green;
+      //}
     }
-  }
-  .eng {
-    b {
+    .eng > .block > b {
       color: green;
     }
+
+    .block {font-size: 1.5rem;}
+
+    .front, .back {
+      b {
+        text-decoration: underline;
+      }
+    }
+    .prj {
+      //display: flex;
+      //justify-content: center;
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+
+      b {
+        color: darkred;
+      }
+    }
+
+    .prj:hover {
+      .block {
+        background: lightblue;
+        box-shadow: 3px 3px 4px 0 lightgrey;
+        border-color: lightskyblue;
+      }
+
+      b {
+        color: red;
+        text-decoration: none;
+      }
+    }
   }
 }
 
-.prj {
-  display: flex;
-  justify-content: center;
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-  b {
-    color: darkred;
+@media (max-width: 768px) {
+  .certificates {
+    .container {
+      display: grid;
+      justify-items: center;
+      grid-template-columns: 1fr;
+      //grid-template-rows: auto;
+      grid-template-areas:
+      "back"
+      "front"
+      "database"
+      "designer"
+      "pm"
+      "other"
+      "eng";
+      grid-column-gap: 0.5rem;
+      grid-row-gap: 0;
+      grid-auto-flow: column;
+      //.prj {
+      //  display: flex;
+      //  justify-content: center;
+      //}
+
+      .block {font-size: 1.2rem;}
+
+      .eng {
+        b {
+          color: green;
+        }
+      }
+    }
   }
 }
 
-.prj:hover {
-  .block {
-    background: lightblue;
-    box-shadow: 3px 3px 4px 0 lightgrey;
-    border-color: lightskyblue;
-  }
 
-  b {
-    color: red;
-    text-decoration: none;
-  }
-}
 </style>
