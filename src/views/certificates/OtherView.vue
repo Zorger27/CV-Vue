6ce8ac9c92a773fde75f8f1b35c88b573f272028
Anatolii Zorin <Zorger27@GMail.com>
2023-03-28 00:10:04 +0300

    Google certificates

--- src/views/certificates/OtherView.vue
+++ src/views/certificates/OtherView.vue
@@ -18,9 +18,9 @@ export default class other extends Vue {}
     <line></line>
       <div v-for="sert in otherStore.state.otherStore" class="certificate">
         <a class="block" :href="sert.image" title="Certificate..." target="_blank">
-          <h3>{{ sert.id }}. {{ sert.title }}</h3>
+          <h3>{{ sert.id }}. {{ this.$i18n.locale === "ua" ? sert.titleua : sert.title }}</h3>
           <div>{{ $t('cert.number') }}<strong>{{ sert.regnumber }}</strong></div>
-          <div>{{ $t('cert.grade') }}<strong>{{ sert.grade }}</strong></div>
+          <div>{{ $t('cert.grade') }}<strong>{{ this.$i18n.locale === "ua" ? sert.gradeua : sert.grade }}</strong></div>
           <div>{{ $t('cert.date') }}{{ sert.examdate }}</div>
         </a>
       </div>
