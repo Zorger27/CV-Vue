48a93b99321400ab4b03538ba53f8c76202ac53c
Anatolii Zorin <Zorger27@GMail.com>
2023-04-09 00:09:08 +0300

    Small changes

--- src/views/extra/CalculatorView.vue
+++ src/views/extra/CalculatorView.vue
@@ -1,126 +1,114 @@
-<script>
+<script lang="ts">
 import {Options, Vue} from "vue-class-component";
+import { reactive } from 'vue';
 
-@Options({components: {},})
+interface CalculatorState {
+  input: string;
+  numbers: string[];
+  operators: string[];
+  resultDisplayed: boolean;
+}
+
+@Options({
+  computed: {},
+  components: {},
+})
 export default class CalculatorView extends Vue {
-//   // объявляем переменные (кнопки калькулятора)
-//   input = document.querySelector('.input')
-//   number = document.querySelectorAll('.numbers div')
-//   operator = document.querySelectorAll(['.operators div','.specific'])
-//   result = document.querySelector('.equal')
-//   clear = document.querySelector('.clear')
-//   resultDisplayed = false; //смотрим, что отображается
-//
-// // добавляем обработчик кликов к цифровым кнопкам
-//   for( i = 0; i < number.length; i++){
-//   number[i].addEventListener("click", function (e){
-//     // сохраненим текущую входящую строку и её последний символ в переменных (используем позже)
-//     let currentString = input.innerHTML;
-//     let lastChar = currentString[currentString.length - 1];
-//     // если результат не отображается, просто продолжаем добавлять
-//     if (resultDisplayed === false){
-//       input.innerHTML += e.target.innerHTML;
-//     }
-//     else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "√" || lastChar === "^"){
-//       // если в данный момент отображается результат и пользователь нажал оператор,
-//       // то нам нужно продолжать добавлять в строку для следующей операции
-//       resultDisplayed = false;
-//       input.innerHTML += e.target.innerHTML;
-//     }
-//     else {
-//       // если в данный момент отображается результат и пользователь нажал число
-//       // нам нужно очистить входную строку и добавить новый ввод, чтобы начать новую операцию
-//       resultDisplayed = false;
-//       input.innerHTML = "";
-//       input.innerHTML += e.target.innerHTML;
-//     }
-//   });
-// }
-// // добавляем обработчик кликов к операторным кнопкам
-// for (let i = 0; i < operator.length; i++){
-//   operator[i].addEventListener("click", function (e){
-//     // сохраненим текущую входящую строку и её последний символ в переменных (используем позже)
-//     let currentString = input.innerHTML;
-//     let lastChar = currentString[currentString.length - 1];
-//     // если последний введенный символ является оператором, заменим его текущим нажатием
-//     if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷" || lastChar === "√" || lastChar === "^"){
-//       input.innerHTML = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-//     }
-//     else if (currentString.length === 0){
-//       // если первая нажатая клавиша является оператором - ничего не делаем
-//       console.log("Enter a number first!");
-//     }
-//     else {
-//       // иначе просто добавим оператор нажатия
-//       input.innerHTML += e.target.innerHTML;
-//     }
-//   });
-// }
-// // добавляем обработчик клика по нажатию на кнопку "равно"
-// result.addEventListener("click", function (){
-//   // это строка, которую мы будем обрабатывать, например: -40+26+33-56*34/22
-//   let inputString = input.innerHTML;
-//   // создаём массив чисел; например, для приведенной выше строки это будет: numbers = ["40", "26", "33", "56", "34", "22"]
-//   let numbers = inputString.split(/[√÷×^\-+]/g);
-//   // создаём массив операторов; для вышеуказанной строки это будет: operator = ["√" "÷", "*", "^", "-", "+"]
-//   // сначала мы заменяем все числа и точки пустой строкой, а затем разделяем
-//   let operators = inputString.replace(/\d|\./g, "").split("");
-//
-//   console.log(inputString);
-//   console.log(operators);
-//   console.log(numbers);
-//   console.log("---------------------------");
-//
-//   // теперь мы перебираем массив и выполняем одну операцию за раз
-//   // сначала делим, потом умножаем, потом вычитаем и потом складываем
-//   // по мере движения мы меняем исходный массив чисел и операторов
-//   // последний элемент, оставшийся в массиве, будет выводом
-//
-//   let square = operators.indexOf("√");
-//   while (square !== -1){
-//     numbers.splice(square, 2, numbers[square] ** (1/2));
-//     operators.splice(square, 1);
-//     square = operators.indexOf("√");
-//   }
-//   let divide = operators.indexOf("÷");
-//   while (divide !== -1){
-//     numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-//     operators.splice(divide, 1);
-//     divide = operators.indexOf("÷");
-//   }
-//   let caret = operators.indexOf("^");
-//   while (caret !== -1){
-//     numbers.splice(caret, 2, numbers[caret] * numbers[caret]);
-//     operators.splice(caret, 1);
-//     caret = operators.indexOf("^");
-//   }
-//   let multiply = operators.indexOf("×");
-//   while (multiply !== -1){
-//     numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-//     operators.splice(multiply, 1);
-//     multiply = operators.indexOf("×");
-//   }
-//   let subtract = operators.indexOf("-");
-//   while (subtract !== -1) {
-//     numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-//     operators.splice(subtract, 1);
-//     subtract = operators.indexOf("-");
-//   }
-//   let add = operators.indexOf("+");
-//   while (add !== -1){
-//     // используем parseFloat, иначе это приведет к конкатенации строк
-//     numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add +1]));
-//     operators.splice(add, 1);
-//     add = operators.indexOf("+");
-//   }
-//   input.innerHTML = numbers[0]; // отображение вывода
-//   resultDisplayed = true; // меняем значение на true, если отображается результат
-// });
-//
-// // очищаем поле ввода, при нажатии на кнопку очистки
-// clear.addEventListener("click", function (){
-//   input.innerHTML = "";
-// });
+  showContent = false;
+
+  setup() {
+    const state = reactive<CalculatorState>({
+      input: '',
+      numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'],
+      operators: ['+', '-', '×', '÷'],
+      resultDisplayed: false,
+    });
+    const addNumber = (number: string) => {
+      let currentString = state.input;
+      let lastChar = currentString[currentString.length - 1];
+      if (state.resultDisplayed === false) {
+        state.input += number;
+      } else if (
+          state.resultDisplayed &&
+          (lastChar === '+' ||
+              lastChar === '-' ||
+              lastChar === '×' ||
+              lastChar === '÷' ||
+              lastChar === '√' ||
+              lastChar === '^')
+      ) {
+        state.resultDisplayed = false;
+        state.input += number;
+      } else {
+        state.resultDisplayed = false;
+        state.input = '';
+        state.input += number;
+      }
+    };
+    const addOperator = (operator: string) => {
+      let currentString = state.input;
+      let lastChar = currentString[currentString.length - 1];
+      if (
+          lastChar === '+' ||
+          lastChar === '-' ||
+          lastChar === '×' ||
+          lastChar === '÷' ||
+          lastChar === '√' ||
+          lastChar === '^'
+      ) {
+        state.input = currentString.substring(0, currentString.length - 1) + operator;
+      } else if (currentString.length === 0) {
+        console.log('Enter a number first!');
+      } else {
+        state.input += operator;
+      }
+    };
+
+    const clear = () => {
+      state.input = '';
+    };
+  }
+    // const resultClicked = () => {
+    //   const resultElement = document.getElementById("result");
+    //   const expression = resultElement.value;
+    //   const [numbers, operators] = parseExpression(expression);
+    //
+    //   const calculate = (numbers, operators) => {
+    //     const priority1 = ["*", "/", "%"];
+    //     const priority2 = ["+", "-"];
+    //
+    //     for (const operator of priority1) {
+    //       let i = 0;
+    //       while (i < operators.length) {
+    //         if (operators[i] === operator) {
+    //           const result = eval(`${numbers[i]}${operator}${numbers[i + 1]}`);
+    //           numbers.splice(i, 2, result);
+    //           operators.splice(i, 1);
+    //         } else {
+    //           i++;
+    //         }
+    //       }
+    //     }
+    //
+    //     for (const operator of priority2) {
+    //       let i = 0;
+    //       while (i < operators.length) {
+    //         if (operators[i] === operator) {
+    //           const result = eval(`${numbers[i]}${operator}${numbers[i + 1]}`);
+    //           numbers.splice(i, 2, result);
+    //           operators.splice(i, 1);
+    //         } else {
+    //           i++;
+    //         }
+    //       }
+    //     }
+    //
+    //     return numbers[0];
+    //   };
+    //
+    //   const result = calculate(numbers, operators);
+    //   resultElement.value = result.toString();
+    // };
 }
 </script>
 
@@ -130,9 +118,29 @@ export default class CalculatorView extends Vue {
       <router-link class="back" to="/extra" title="Back to Extra page"><i class="fa fa-arrow-circle-left"></i>
       </router-link>
       {{ $t('extra.h1m.calculator') }}
+      <a title="Other view"><i @click="showContent = !showContent" class="fa fa-arrow-circle-down"></i></a>
     </h1>
     <line></line>
-    <div class="body">
+    <div v-if="showContent === true" class="container">
+      <div class="input">{{ input }}</div>
+      <div class="calculator">
+        <div class="row">
+          <div class="numbers">
+            <div v-for="number in numbers" :key="number" @click="addNumber(number)">{{ number }}</div>
+          </div>
+          <div class="operators">
+            <div v-for="operator in operators" :key="operator" @click="addOperator(operator)">{{ operator }}</div>
+            <div class="specific" @click="addOperator('^')">^</div>
+            <div class="specific" @click="addOperator('√')">√</div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="clear" @click="clear()">C</div>
+          <div class="equal" @click="calculate()">=</div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="body">
       <div class="calculator">
         <div class="input"></div>
         <div class="buttons">
--- src/views/extra/WeatherView.vue
+++ src/views/extra/WeatherView.vue
@@ -1,8 +1,49 @@
 <script lang="ts">
+import axios from "axios";
 import {Options, Vue} from "vue-class-component";
+
+export interface Weather {
+  weather: {
+    description: string;
+    icon: string;
+    temperature: number;
+    feelsLike: number;
+    humidity: number;
+    windSpeed: number;
+  };
+}
 
 @Options({components: {},})
 export default class WeatherView extends Vue {
+
+  error: string | null | undefined;
+  loading: boolean | undefined;
+  weather: any;
+
+
+  data() {
+    return {
+      loading: true,
+      weather: null,
+      error: null
+    };
+  }
+    mounted()
+    {
+      axios
+          .get(
+              "https://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&lang=ru&appid=YOUR_API_KEY"
+          )
+          .then(response => {
+            this.weather = response.data;
+          })
+          .catch(error => {
+            this.error = "Не удалось загрузить данные о погоде.";
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+    }
 }
 </script>
 
@@ -14,6 +55,19 @@ export default class WeatherView extends Vue {
       {{ $t('extra.h1m.weather') }}
     </h1>
     <line></line>
+    <div>
+      <h1>Погода в Киеве</h1>
+      <div v-if="loading">Загрузка...</div>
+      <div v-if="error">{{ error }}</div>
+      <div v-if="weather">
+        <p>Температура: {{ weather.main.temp }}°C</p>
+        <p>Ощущается как: {{ weather.main.feels_like }}°C</p>
+        <p>Скорость ветра: {{ weather.wind.speed }} м/с</p>
+        <p>Направление ветра: {{ weather.wind.deg }}°</p>
+        <p>Влажность: {{ weather.main.humidity }}%</p>
+        <p>Давление: {{ weather.main.pressure }} гПа</p>
+      </div>
+    </div>
   </div>
 </template>
 
