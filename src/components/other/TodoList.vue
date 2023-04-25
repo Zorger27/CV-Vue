d9d961286331a684984d2f122ddd325b94a66e30
Anatolii Zorin <Zorger27@GMail.com>
2023-04-25 03:25:51 +0300

    Todo list

--- src/components/other/TodoList.vue
+++ src/components/other/TodoList.vue
@@ -46,9 +46,16 @@ interface Task {
       this.saveTasks();
       localStorage.removeItem('tasks');
     },
+    toggleCompleted(task: Task) {
+      task.completed = !task.completed;
+      this.saveTasks();
+    },
     loadTasks() {
       const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-      this.tasks = tasks;
+      this.tasks = tasks.map((task: Task) => {
+        task.completed = task.completed || false; // добавляем проверку для того, чтобы значение по умолчанию было false
+        return task;
+      });
       this.lastId = tasks.reduce((maxId: number, task: Task) => Math.max(maxId, task.id), 0);
     },
     saveTasks() {
@@ -69,14 +76,15 @@ export default class TodoList extends Vue {
         <th colspan="4">
           <form @submit.prevent="addTask">
             <input type="text" v-model="newTask" :placeholder="$t('extra.todo.add-task')"/>
-            <button class="btn norm">{{ $t('extra.todo.add') }}</button>
+            <!--            <textarea v-model="newTask" :placeholder="$t('extra.todo.add-task')"/>-->
+            <button class="add-task">+</button>
           </form>
         </th>
       </tr>
       <tr>
         <th>№</th>
         <th>{{ $t('extra.todo.task') }}</th>
-        <th class="status">{{ $t('extra.todo.status') }}</th>
+        <th><span class="icon"><i class="fas fa-check-circle"></i></span><span class="txt">{{ $t('extra.todo.status') }}</span></th>
         <th>{{ $t('extra.todo.actions') }}</th>
       </tr>
       </thead>
@@ -90,13 +98,19 @@ export default class TodoList extends Vue {
           <span v-else v-bind:class="{ completed: task.completed }">{{ task.name }}</span>
         </td>
         <td>
-          <button class="btn" @click="task.completed = !task.completed">
+          <button class="btn" @click="toggleCompleted(task)" title="Status...">
             <i :class="task.completed ? 'fas fa-check' : 'fas fa-clock'"></i>
           </button>
         </td>
         <td class="action">
-          <button class="btn norm" @click="task.editing = true" v-if="!task.editing">{{ $t('extra.todo.edit') }}</button>
-          <button class="btn danger" @click="deleteTask(index)">{{ $t('extra.todo.delete') }}</button>
+          <button class="btn norm txt" @click="task.editing = true" v-if="!task.editing">{{ $t('extra.todo.edit') }}</button>
+          <button class="btn danger txt" @click="deleteTask(index)">{{ $t('extra.todo.delete') }}</button>
+          <button class="btn norm icon" @click="task.editing = true" v-if="!task.editing" title="Edit...">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button class="btn danger icon" @click="deleteTask(index)" title="Delete...">
+            <i class="fas fa-trash"></i>
+          </button>
         </td>
       </tr>
       </tbody>
@@ -126,6 +140,9 @@ export default class TodoList extends Vue {
     word-wrap: break-word; /* перенос слов */
   }
 
+  .icon {display: none;}
+  .txt {display: inline-block;}
+
   .action {
     max-width: 250px;
     word-wrap: break-word;
@@ -141,7 +158,7 @@ export default class TodoList extends Vue {
     border: 1px solid #e0e0e0;
     font-size: 2rem;
     border-radius: 5px;
-    width: 63%;
+    width: 75%;
     padding: 0.6rem;
     margin-right: 5px;
   }
@@ -156,47 +173,49 @@ export default class TodoList extends Vue {
     margin: 5px 5px;
     cursor: pointer;
     transition: border-color .2s ease-in-out, background-color .2s, box-shadow .2s;
+    .fa-check {color: darkgoldenrod;}
+    .fa-clock {color: darkred;}
   }
 
   .btn:hover {
-    background-color: #f1f1f1;
+    background-color: white;
     box-shadow: 3px 3px 4px 0 lightgrey;
   }
 
-  .danger {
-    color: darkred;
+  .danger {color: darkred;}
+  .danger:hover {color: red;background-color: white;}
+  .norm {color: lightseagreen;}
+  .norm:hover {color: limegreen;background-color: white;}
+
+  .add-task {
+    width: 3rem;
+    height: 3rem;
+    border: 1px solid steelblue;
+    border-radius: 50%;
+    background: steelblue;
+    margin-left: 5px;
+    color: white;
+    font-size: 2rem;
+    font-weight: bolder;
+    text-align: center;
+    cursor: pointer;
+    transition: border-color .2s ease-in-out, background-color .2s, box-shadow .2s;
   }
 
-  .danger:hover {
-    color: red;
-    background-color: white;
-  }
-
-  .norm {
-    color: lightseagreen;
-  }
-
-  .norm:hover {
-    color: limegreen;
-    background-color: white;
-  }
+  .add-task:hover {box-shadow: 3px 3px 4px 0 lightgrey;}
 }
 
 @media(max-width: 1020px) {
   .table {
     font-size: 1.8rem;
-
-    .name {
-      max-width: 355px;
-    }
-
-    .action {
-      max-width: 180px;
-    }
+    .name {max-width: 355px;}
+    .txt {display: inline-block;}
+    .icon {display: none;}
+    .action {max-width: 180px;}
 
     input[type="text"] {
       font-size: 1.8rem;
-      width: 73%;
+      width: 75%;
       padding: 0.5rem;
       margin-right: 5px;
     }
@@ -206,24 +225,25 @@ export default class TodoList extends Vue {
       padding: 0.5rem;
       margin: 5px 5px;
     }
+
+    .add-task {
+      width: 2.8rem;
+      height: 2.8rem;
+      margin-left: 5px;
+      font-size: 1.8rem;
+    }
   }
 }
 
 @media (max-width: 768px) {
   .table {
     font-size: 1rem;
-
-    .name {
-      max-width: 100px;
-    }
-
-    .action {
-      max-width: 150px;
-    }
+    .name {max-width: 170px;}
+    .action {max-width: 150px;}
 
     input[type="text"] {
       font-size: 1rem;
-      width: 60%;
+      width: 75%;
       padding: 0.3rem;
       margin-right: 3px;
     }
@@ -234,11 +254,18 @@ export default class TodoList extends Vue {
       margin: 3px 3px;
     }
 
-    .status {
-      visibility: hidden;
-      margin: 0;
-      //transform: rotate(-90deg);
-      max-width: 20px;
+    .add-task {
+      width: 1.8rem;
+      height: 1.8rem;
+      margin-left: 3px;
+      font-size: 1rem;
+    }
+
+    .txt {display: none;}
+    .icon {display: inline-block;}
+
+    thead {
+      .fa-check-circle {color: steelblue;}
     }
   }
 }
