7bc76126fbe25e666211be3f6f049192397b0fcc
Anatolii Zorin <Zorger27@GMail.com>
2023-05-02 23:48:24 +0300

    Fizzbuzz

--- src/views/tasks/Fizzbuzz.vue
+++ src/views/tasks/Fizzbuzz.vue
@@ -20,6 +20,7 @@ export default class Fizzbuzz extends Vue {
       }
     }
   }
+
   clearNumbers(): void {
     this.numbers = [];
     this.number = 1;
@@ -42,11 +43,11 @@ export default class Fizzbuzz extends Vue {
     <div>
       <form @submit.prevent="generateNumbers">
         <label>
-          Введите число:
+          {{ $t('extra.tasks.fizz-des05') }}
           <input type="number" v-model.number="number" min="1" required>
         </label>
-        <button type="submit">Генерировать</button>
-        <button type="button" @click="clearNumbers" v-if="numbers">Очистить</button>
+        <button type="submit" class="btn">{{ $t('extra.tasks.fizz-des06') }}</button>
+        <button type="button" class="btn" @click="clearNumbers" v-if="numbers">{{ $t('extra.tasks.fizz-des07') }}</button>
       </form>
       <div class="numbers" v-if="numbers">
         <span v-for="num in numbers" :key="num"
@@ -60,7 +61,7 @@ export default class Fizzbuzz extends Vue {
       </a>
     </div>
     <div class="certificate">
-      <a class="block" href=# title="Download solution in pdf format..."
+      <a class="block" href="/assets/tasks/FizzBuzz-Vue.pdf" title="Download solution in pdf format..."
          target="_blank"><h3>{{ $t('extra.tasks.link-vue') }}</h3>
       </a>
     </div>
@@ -70,6 +71,7 @@ export default class Fizzbuzz extends Vue {
 <style lang="scss" scoped>
 .fizzbuzz {
   flex: 1 0 auto;
+  font-size: 1.6rem;
 
   .back {
     display: inline-flex;
@@ -77,13 +79,42 @@ export default class Fizzbuzz extends Vue {
     margin-right: 0.1rem;
   }
 
-  .numbers {
-    white-space: pre-wrap;
+  input[type="number"] {
+    flex: 1 0 auto;
+    caret-color: red;
+    border: 1px solid lightskyblue;
+    font-size: 1.6rem;
+    border-radius: 5px;
+    width: 100px;
+    padding: 0.6rem;
+    margin: 5px 5px 10px 0;
   }
 
-  .numbers span {
-    display: inline-block;
-    margin-right: 5px;
+  input:active, :focus {
+    outline: 1px solid lightskyblue;
+    box-shadow: 3px 3px 4px 0 lightgrey;
+  }
+  .btn {
+    border: 1px solid rgba(112, 111, 111, 0.6);
+    border-radius: 5px;
+    padding: 0.6rem;
+    margin: 0.5rem;
+    box-shadow: 3px 3px 4px 0 lightgrey;
+  }
+  .btn:hover, :focus {
+    border: 1px solid lightskyblue;
+    box-shadow: 3px 3px 4px 0 lightblue;
+    background: none;
+    color: blue;
+  }
+
+  .numbers {
+    white-space: pre-wrap;
+
+    span {
+      display: inline-block;
+      margin-right: 5px;
+    }
   }
 
   .fizz {
@@ -102,18 +133,27 @@ export default class Fizzbuzz extends Vue {
     text-align: center;
   }
 
-
-  p {
+  p, .btn {
     font-size: 1.6rem;
   }
 
   @media (max-width: 1020px) {
-    p {
+    font-size: 1.3rem;
+    input[type="number"] {
+      font-size: 1.3rem;
+      width: 85px;
+    }
+    p, .btn {
       font-size: 1.3rem;
     }
   }
   @media (max-width: 768px) {
-    p {
+    font-size: 1rem;
+    input[type="number"] {
+      font-size: 1rem;
+      width: 65px;
+    }
+    p, .btn {
       font-size: 1rem;
     }
   }
