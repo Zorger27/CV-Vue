a57055f5803762f89c2057560d240fb8ec210e77
Anatolii Zorin <Zorger27@GMail.com>
2023-06-29 04:00:12 +0300

    Pixabay API

--- src/views/certificates/CertificatesView.vue
+++ src/views/certificates/CertificatesView.vue
@@ -1,7 +1,21 @@
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+  // data() {return {searchValue: "",};},
+  // methods: {clearSearch() {this.searchValue = "";}},
+  components: {},
+})
+export default class Certificates extends Vue {}
+</script>
+
 <template>
   <div class="certificates">
     <h1>{{ $t('cert.title-main') }}</h1>
     <line></line>
+<!--    <div class="search-bar">-->
+<!--      <input type="text" :placeholder="$t('cert.search')" v-model="searchValue"><i title="Clear" @click="clearSearch" class="fas fa-trash-alt"></i>-->
+<!--    </div>-->
     <div class="container">
       <div class="prj back">
         <router-link class="block" to="/certificates/backend">
@@ -14,6 +28,7 @@
       </div>
       <div class="prj front">
         <router-link class="block" to="/certificates/frontend">
+        <!--        <router-link class="block" to="/certificates/frontend" :search="searchValue">-->
           <b>{{ $t('cert.front.frontend') }}</b><br>
           <i>{{ $t('cert.front.html') }}<br>
             {{ $t('cert.front.javascript') }}<br>
@@ -41,20 +56,37 @@
   </div>
 </template>
 
-<script lang="ts">
-import {Vue} from "vue-class-component";
-
-export default class Certificates extends Vue {
-}
-
-</script>
-
 <style lang="scss" scoped>
 .certificates {
   flex: 1 0 auto;
   background: linear-gradient(to bottom, rgb(247, 234, 250), rgb(255, 250, 229)) no-repeat center;
 
-  .container {
+  .search-bar {
+    margin: 1rem auto;
+    font-size: 1.6rem;
+
+    input {
+      border: 1px solid lightskyblue;
+      border-radius: 5px;
+      font-size: 1.6rem;
+      transition: border-color .2s ease-in-out, background-color .2s, box-shadow .2s;
+    }
+
+    input:active, :focus {
+      outline: 1px solid lightskyblue;
+      outline-offset: 0;
+      box-shadow: 3px 3px 4px 0 grey;
+    }
+    .fa-trash-alt {
+      font-size: 1.6rem;
+      margin-left: 7px;
+    }
+    .fa-trash-alt:hover {
+      color: hotpink;
+    }
+  }
+
+    .container {
     display: inline-grid;
     justify-items: center;
     align-items: center;
@@ -120,6 +152,19 @@ export default class Certificates extends Vue {
 }
 
 @media (max-width: 768px) {
+  .search-bar {
+    margin: 0.3rem auto;
+    font-size: 1.3rem;
+
+    input[type="text"] {
+      border-radius: 3px;
+      font-size: 1.3rem;
+    }
+    .fa-trash-alt {
+      font-size: 1.3rem;
+    }
+
+  }
   .certificates {
     h1 {
       font-size: 1.7rem;
@@ -156,6 +201,5 @@ export default class Certificates extends Vue {
     }
   }
 }
-
 
 </style>
