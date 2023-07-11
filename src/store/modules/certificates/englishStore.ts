c738314452af00842cbba6999ecca1f8c9ac69bd
Anatolii Zorin <Zorger27@GMail.com>
2023-07-12 01:20:06 +0300

    Small changes

--- src/store/modules/certificates/englishStore.ts
+++ src/store/modules/certificates/englishStore.ts
@@ -3,6 +3,18 @@ const state = {
     [
       {
         id: 1,
+        title_en: "English for kids",
+        title_ua: "Англійська для дітей",
+        title_es: "Inglés para niños",
+        regnumber: "Starter",
+        grade_en: "Successfully completed",
+        grade_ua: "Успішно завершено",
+        grade_es: "Completado con éxito",
+        examdate: "11.07.2023",
+        image: "https://www.englishdom.com/ua/cn/55a98dad"
+      },
+      {
+        id: 2,
         title_en: "English for beginners",
         title_ua: "Англійська для початківців",
         title_es: "Inglés para principiantes",
@@ -14,7 +26,7 @@ const state = {
         image: "https://www.englishdom.com/cn/a7aad5cd"
       },
       {
-        id: 2,
+        id: 3,
         title_en: "Conversational English",
         title_ua: "Розмовна англійська",
         title_es: "Inglés conversacional",
@@ -26,7 +38,7 @@ const state = {
         image: "https://www.englishdom.com/cn/c100d53f"
       },
       {
-        id: 3,
+        id: 4,
         title_en: "English for traveling",
         title_ua: "Англійська для подорожей",
         title_es: "Inglés para viajar",
@@ -38,7 +50,7 @@ const state = {
         image: "https://www.englishdom.com/cn/e9e057a3"
       },
       {
-        id: 4,
+        id: 5,
         title_en: "English for IT",
         title_ua: "Англійська для ІТ",
         title_es: "Inglés para TI",
@@ -50,7 +62,7 @@ const state = {
         image: "https://www.englishdom.com/cn/808f94fa"
       },
       {
-        id: 5,
+        id: 6,
         title_en: "Interview in English",
         title_ua: "Співбесіда англійською мовою",
         title_es: "Entrevista en Inglés",
@@ -62,7 +74,7 @@ const state = {
         image: "https://www.englishdom.com/cn/478f8e14"
       },
       {
-        id: 6,
+        id: 7,
         title_en: "Business English",
         title_ua: "Ділова англійська",
         title_es: "Inglés de negocios",
@@ -74,7 +86,7 @@ const state = {
         image: "https://www.englishdom.com/cn/0600cfd1"
       },
       {
-        id: 7,
+        id: 8,
         title_en: "Intermediate level of English",
         title_ua: "Середній рівень англійської мови",
         title_es: "Nivel intermedio de inglés",
@@ -86,7 +98,7 @@ const state = {
         image: "https://www.englishdom.com/cn/9d6c54e8"
       },
       {
-        id: 8,
+        id: 9,
         title_en: "Irregular Verbs",
         title_ua: "Неправильні дієслова",
         title_es: "Verbos irregulares",
@@ -98,7 +110,7 @@ const state = {
         image: "https://www.englishdom.com/cn/21258ee6"
       },
       {
-        id: 9,
+        id: 10,
         title_en: "English with native speaker",
         title_ua: "Англійська з носієм мови",
         title_es: "Inglés con hablante nativo",
@@ -110,7 +122,7 @@ const state = {
         image: "https://www.englishdom.com/cn/7d490f31"
       },
       {
-        id: 10,
+        id: 11,
         title_en: "English for advanced",
         title_ua: "Англійська Поглиблена",
         title_es: "Inglés para avanzados",
