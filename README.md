feac4ace8124c0d2dbe26b009918fc4a514c6762
Anatolii Zorin <Zorger27@GMail.com>
2023-08-27 02:01:23 +0300

    Small changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MY Portfolio (SPA) #
 ### You can view my CV (Landing Page) here: [Curriculum VITAE](https://zorger27.github.io)❗️ ###
 ### You can view my Portfolio (SPA) here: [My Portfolio](https://Zorin.Expert)❗️ ###
-### Technologies: [Vue.JS v.3.2](https://v3.ru.vuejs.org), [Vuex](https://vuex.vuejs.org), HTML5, CSS3, FlexBox CSS, Grid CSS, JavaScript, TypeScript. ###
+### Technologies: [Vue.js v.3.2](https://v3.ru.vuejs.org), [Vuex](https://vuex.vuejs.org), HTML5, CSS3, FlexBox CSS, Grid CSS, JavaScript, TypeScript. ###
 ![02 Screen 06-03-2023](https://user-images.githubusercontent.com/30940416/223004436-afa889bb-deba-4036-ab2f-f686a962a6bb.gif)
 
 ## Project setup
--- src/store/modules/certificates/englishStore.ts
+++ src/store/modules/certificates/englishStore.ts
@@ -63,6 +63,18 @@ const state = {
       },
       {
         id: 6,
+        title_en: "Preparation for the school exam",
+        title_ua: "Підготовка до шкільного іспиту",
+        title_es: "Preparación para el examen escolar",
+        regnumber: "Pre-Intermediate",
+        grade_en: "Successfully completed",
+        grade_ua: "Успішно завершено",
+        grade_es: "Completado con éxito",
+        examdate: "26.08.2023",
+        image: "https://www.englishdom.com/cn/60aacf23"
+      },
+      {
+        id: 7,
         title_en: "English for traveling",
         title_ua: "Англійська для подорожей",
         title_es: "Inglés para viajar",
@@ -74,7 +86,7 @@ const state = {
         image: "https://www.englishdom.com/cn/e9e057a3"
       },
       {
-        id: 7,
+        id: 8,
         title_en: "English for IT",
         title_ua: "Англійська для ІТ",
         title_es: "Inglés para TI",
@@ -86,7 +98,7 @@ const state = {
         image: "https://www.englishdom.com/cn/808f94fa"
       },
       {
-        id: 8,
+        id: 9,
         title_en: "Interview in English",
         title_ua: "Співбесіда англійською мовою",
         title_es: "Entrevista en Inglés",
@@ -98,7 +110,7 @@ const state = {
         image: "https://www.englishdom.com/cn/478f8e14"
       },
       {
-        id: 9,
+        id: 10,
         title_en: "Business English",
         title_ua: "Ділова англійська",
         title_es: "Inglés de negocios",
@@ -110,7 +122,7 @@ const state = {
         image: "https://www.englishdom.com/cn/0600cfd1"
       },
       {
-        id: 10,
+        id: 11,
         title_en: "Intermediate level of English",
         title_ua: "Середній рівень англійської мови",
         title_es: "Nivel intermedio de inglés",
@@ -122,7 +134,7 @@ const state = {
         image: "https://www.englishdom.com/cn/9d6c54e8"
       },
       {
-        id: 11,
+        id: 12,
         title_en: "Intermediate for teenagers",
         title_ua: "Середній рівень для підлітків",
         title_es: "Intermedio para adolescentes",
@@ -134,7 +146,7 @@ const state = {
         image: "https://www.englishdom.com/cn/33038d5f"
       },
       {
-        id: 12,
+        id: 13,
         title_en: "Irregular Verbs",
         title_ua: "Неправильні дієслова",
         title_es: "Verbos irregulares",
@@ -146,7 +158,7 @@ const state = {
         image: "https://www.englishdom.com/cn/21258ee6"
       },
       {
-        id: 13,
+        id: 14,
         title_en: "English with native speaker",
         title_ua: "Англійська з носієм мови",
         title_es: "Inglés con hablante nativo",
@@ -158,7 +170,7 @@ const state = {
         image: "https://www.englishdom.com/cn/7d490f31"
       },
       {
-        id: 14,
+        id: 15,
         title_en: "English for advanced",
         title_ua: "Англійська Поглиблена",
         title_es: "Inglés para avanzados",
