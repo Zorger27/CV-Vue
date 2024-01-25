56dfd422de9e3b166f88caa5d5bf402a7deaec3f
Anatolii Zorin <Zorger27@GMail.com>
2024-01-25 04:09:28 +0200

    Small changes.

--- src/store/modules/service/infoStore.ts
+++ src/store/modules/service/infoStore.ts
@@ -27,84 +27,96 @@ const state = {
       },
       {
         id: 5,
+        title: "Three.js",
+        version: "160",
+        url: "https://github.com/mrdoob/three.js/releases"
+      },
+      {
+        id: 6,
+        title: "Orbit controls",
+        version: "82.1.0",
+        url: "https://threejs.org/docs/#examples/en/controls/OrbitControls"
+      },
+      {
+        id: 7,
         title: "Axios",
         version: "1.6.2",
         url: "https://axios-http.com"
       },
       {
-        id: 6,
+        id: 8,
         title: "HTML5",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/HTML/Introduction_to_HTML"
       },
       {
-        id: 7,
+        id: 9,
         title: "CSS3",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/First_steps"
       },
       {
-        id: 8,
+        id: 10,
         title: "FlexBox CSS",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox"
       },
       {
-        id: 9,
+        id: 11,
         title: "Grid CSS",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Web/CSS/CSS_Grid_Layout"
       },
       {
-        id: 10,
+        id: 12,
         title: "JavaScript",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/JavaScript"
       },
       {
-        id: 11,
+        id: 13,
         title: "TypeScript",
         version: "4.5.5",
         url: "https://www.typescriptlang.org/"
       },
       {
-        id: 12,
+        id: 14,
         title: "Google Search Console",
         version: "",
         url: "https://search.google.com/search-console"
       },
       {
-        id: 13,
+        id: 15,
         title: "Google Analytics",
         version: "4.0",
         url: "https://analytics.google.com"
       },
       {
-        id: 14,
+        id: 16,
         title: "OpenWeather Map API",
         version: "",
         url: "https://openweathermap.org"
       },
       {
-        id: 15,
+        id: 17,
         title: "CoinGecko API",
         version: "",
         url: "https://www.coingecko.com"
       },
       {
-        id: 16,
+        id: 18,
         title: "NBU API",
         version: "",
         url: "https://portal.bank.gov.ua/uk/8"
       },
       {
-        id: 17,
+        id: 19,
         title: "Lorem Picsum API",
         version: "",
         url: "https://picsum.photos"
       },
       {
-        id: 18,
+        id: 20,
         title: "Pixabay API",
         version: "",
         url: "https://pixabay.com"
