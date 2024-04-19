9f4766c0ffa07c01c78094cd4ed57ab01f9999cd
Anatolii Zorin <Zorger27@GMail.com>
2024-04-19 23:28:35 +0300

    Small changes

--- src/views/menu/SkillsView.vue
+++ src/views/menu/SkillsView.vue
@@ -12,7 +12,7 @@ import {openGraphMixin} from "@/assets/ogimage/openGraphMixin";
       sliderImages: [
         require('@/assets/certificates/other/08_D0000952361.webp'),
         require('@/assets/certificates/pm/03_Critical_Thinking.webp'),
-        require('@/assets/certificates/pm/04_ Strategic_Thinking.webp'),
+        require('@/assets/certificates/pm/04_Strategic_Thinking.webp'),
         require('@/assets/certificates/other/09_TP17348317.webp'),
         require('@/assets/certificates/other/01_TP03316433.webp'),
         require('@/assets/certificates/other/02_TP18727720.webp')
