837838492dd6fbd7a2a2ab26af5d67c351f0fccd
Anatolii Zorin <Zorger27@GMail.com>
2024-08-05 00:53:20 +0300

    Small changes

--- src/assets/ogimage/openGraphMixin.ts
+++ src/assets/ogimage/openGraphMixin.ts
@@ -1,14 +1,17 @@
+interface MetaTag {
+  name?: string;
+  property?: string;
+  content: string;
+}
+
 export const openGraphMixin = {
   methods: {
     setPageTitle(mainTitle: string): void {
-      // Проверка, существует ли уже тег <title>
       let pageTitle = document.querySelector('title');
 
       if (pageTitle) {
-        // Если тег <title> существует, обновить его содержимое
         pageTitle.innerText = mainTitle;
       } else {
-        // Если тег <title> не существует, создать новый
         const newTitleTag = document.createElement('title');
         newTitleTag.innerText = mainTitle;
         document.head.appendChild(newTitleTag);
@@ -16,7 +19,7 @@ export const openGraphMixin = {
     },
 
     setOpenGraphTags(metaDescription: string, title: string, description: string, imageUrl: string, url: string): void {
-      const metaTags = [
+      const metaTags: MetaTag[] = [
         { name: 'description', content: metaDescription },
         { property: 'og:title', content: title },
         { property: 'twitter:title', content: title },
@@ -39,13 +42,11 @@ export const openGraphMixin = {
         }
         meta.setAttribute('content', metaTag.content);
 
-        // Удалить существующие мета-теги с тем же property или name
         const existingMetaTags = document.querySelectorAll(
-          `[property="${metaTag.property}"], [name="${metaTag.name}"]`
+          `[property="${metaTag.property || ''}"], [name="${metaTag.name || ''}"]`
         );
         existingMetaTags.forEach((tag) => tag.remove());
 
-        // Добавить новый мета-тег
         document.head.appendChild(meta);
       });
     },
