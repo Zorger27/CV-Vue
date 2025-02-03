e3f4ff3071c9f75e1886e673d74e7620711598e0
Anatolii Zorin <Zorger27@GMail.com>
2025-02-03 02:13:12 +0200

    Small changes

--- src/components/util/MainPhoto.vue
+++ src/components/util/MainPhoto.vue
@@ -1,28 +1,57 @@
 <script lang="ts">
-import {Options, Vue} from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 
 @Options({
   data() {
     return {
-      getPhotoImage: require('@/assets/img/photo/photo1.svg'),
-    }
+      photos: [
+        require('@/assets/img/photo/photo1.svg'),
+        require('@/assets/img/photo/photo2.svg'),
+        require('@/assets/img/photo/photo3.svg'),
+      ],
+      currentPhotoIndex: 1,
+    };
   },
-  mounted() {},
-  methods: {},
-  components: {},
+  mounted() {
+    this.loadPhotoFromLocalStorage();
+  },
+  methods: {
+    changePhoto() {
+      this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.photos.length;
+      this.savePhotoToLocalStorage();
+    },
+    savePhotoToLocalStorage() {
+      localStorage.setItem('selectedPhotoIndex', this.currentPhotoIndex.toString());
+    },
+    loadPhotoFromLocalStorage() {
+      const savedIndex = localStorage.getItem('selectedPhotoIndex');
+      if (savedIndex !== null) {
+        this.currentPhotoIndex = parseInt(savedIndex, 10);
+      }
+    },
+  },
+  computed: {
+    getPhotoImage() {
+      return this.photos[this.currentPhotoIndex];
+    },
+  },
 })
-export default class MainPhoto extends Vue {};
+export default class MainPhoto extends Vue {}
 </script>
 
 <template>
-  <img :src="getPhotoImage" class="photo-image" alt="My Photo Image">
+  <img
+    :src="getPhotoImage"
+    class="photo-image"
+    alt="My Photo Image"
+    @dblclick="changePhoto"
+  />
 </template>
 
 <style scoped lang="scss">
-
 .photo-image {
   width: 100%;
   height: auto;
+  cursor: pointer;
 }
-
 </style>
