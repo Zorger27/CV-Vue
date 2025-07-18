7d1f36072e988ea3524eeb64961372c3c4690560
Anatolii Zorin <Zorger27@GMail.com>
2025-07-18 03:41:44 +0300

    Small changes

--- src/views/menu/HomeView.vue
+++ src/views/menu/HomeView.vue
@@ -76,42 +76,42 @@ export default class Home extends Vue {};
       <div class="photo">
         <MainPhoto></MainPhoto>
       </div>
-      <div class="icons">
-        <!--        <a href="tel:+380504411801" title="Call me: +380 50 441-18-01" target="_blank"><img class=logo-image-->
-        <!--                                                                                            src="@/assets/ico/phone.svg"-->
-        <!--                                                                                            alt="WhatsApp"></a>-->
-        <a href="https://wa.me/380504411801" :title="$t('main.whatsApp')" target="_blank"><img class="logo-image"
-                                                                                                src="@/assets/ico/phone.svg"
-                                                                                                alt="WhatsApp"></a>
-        <a href="viber://chat?number=380504411801" :title="$t('main.viber')" target="_blank"><img class="logo-image"
-                                                                                                      src="@/assets/ico/viber.svg"
-                                                                                                      alt="Viber"></a>
-        <a href="https://t.me/Zorger27" :title="$t('main.telegram')" target="_blank"><img class="logo-image"
-                                                                                              src="@/assets/ico/telegram.svg"
-                                                                                              alt="Telegram"></a>
-        <a href="https://x.com/Regroz" :title="$t('main.twitter')" target="_blank"><img
-          class=logo-image src="@/assets/ico/twitter.svg" alt="Twitter"></a>
-        <a href="https://github.com/Zorger27" :title="$t('main.github')" target="_blank"><img class=logo-image
-                                                                                                src="@/assets/ico/github.svg"
-                                                                                                alt="Github"></a>
-<!--        <a href="https://profile.codersrank.io/user/zorger27/" title="My CodersRank profile..." target="_blank"><img-->
-<!--          class=logo-image src="@/assets/ico/codersrank.svg" alt="CodersRank.io"></a>-->
-        <a href="https://www.pinterest.com/zorger27" :title="$t('main.pinterest')" target="_blank"><img class="logo-image"
-                                                                                            src="@/assets/ico/pinterest.svg"
-                                                                                            alt="Pinterest"></a>
-        <a href="https://www.linkedin.com/in/anatolii-zorin/" :title="$t('main.linkedIn')" target="_blank"><img
-          class=logo-image src="@/assets/ico/linkedin.svg" alt="LinkedIn"></a>
-        <a href="https://www.facebook.com/anatoliy.zorin" :title="$t('main.facebook')" target="_blank"><img
-          class=logo-image src="@/assets/ico/facebook.svg" alt="Facebook"></a>
-        <a href="https://www.youtube.com/c/AnatoliiZorin" :title="$t('main.youTube')" target="_blank"><img
-          class=logo-image src="@/assets/ico/youtube.svg" alt="YouTube"></a>
+<!--      <div class="icons">-->
+<!--        &lt;!&ndash;        <a href="tel:+380504411801" title="Call me: +380 50 441-18-01" target="_blank"><img class=logo-image&ndash;&gt;-->
+<!--        &lt;!&ndash;                                                                                            src="@/assets/ico/phone.svg"&ndash;&gt;-->
+<!--        &lt;!&ndash;                                                                                            alt="WhatsApp"></a>&ndash;&gt;-->
+<!--        <a href="https://wa.me/380504411801" :title="$t('main.whatsApp')" target="_blank"><img class="logo-image"-->
+<!--                                                                                                src="@/assets/ico/phone.svg"-->
+<!--                                                                                                alt="WhatsApp"></a>-->
+<!--        <a href="viber://chat?number=380504411801" :title="$t('main.viber')" target="_blank"><img class="logo-image"-->
+<!--                                                                                                      src="@/assets/ico/viber.svg"-->
+<!--                                                                                                      alt="Viber"></a>-->
+<!--        <a href="https://t.me/Zorger27" :title="$t('main.telegram')" target="_blank"><img class="logo-image"-->
+<!--                                                                                              src="@/assets/ico/telegram.svg"-->
+<!--                                                                                              alt="Telegram"></a>-->
+<!--        <a href="https://x.com/Regroz" :title="$t('main.twitter')" target="_blank"><img-->
+<!--          class=logo-image src="@/assets/ico/twitter.svg" alt="Twitter"></a>-->
+<!--        <a href="https://github.com/Zorger27" :title="$t('main.github')" target="_blank"><img class=logo-image-->
+<!--                                                                                                src="@/assets/ico/github.svg"-->
+<!--                                                                                                alt="Github"></a>-->
+<!--&lt;!&ndash;        <a href="https://profile.codersrank.io/user/zorger27/" title="My CodersRank profile..." target="_blank"><img&ndash;&gt;-->
+<!--&lt;!&ndash;          class=logo-image src="@/assets/ico/codersrank.svg" alt="CodersRank.io"></a>&ndash;&gt;-->
+<!--        <a href="https://www.pinterest.com/zorger27" :title="$t('main.pinterest')" target="_blank"><img class="logo-image"-->
+<!--                                                                                            src="@/assets/ico/pinterest.svg"-->
+<!--                                                                                            alt="Pinterest"></a>-->
+<!--        <a href="https://www.linkedin.com/in/anatolii-zorin/" :title="$t('main.linkedIn')" target="_blank"><img-->
+<!--          class=logo-image src="@/assets/ico/linkedin.svg" alt="LinkedIn"></a>-->
+<!--        <a href="https://www.facebook.com/anatoliy.zorin" :title="$t('main.facebook')" target="_blank"><img-->
+<!--          class=logo-image src="@/assets/ico/facebook.svg" alt="Facebook"></a>-->
+<!--        <a href="https://www.youtube.com/c/AnatoliiZorin" :title="$t('main.youTube')" target="_blank"><img-->
+<!--          class=logo-image src="@/assets/ico/youtube.svg" alt="YouTube"></a>-->
 
-        <a href="mailto:zorger27@gmail.com" :title="$t('main.email')" target="_blank"><img class=logo-image
-                                                                                                      src="@/assets/ico/mail.svg"
-                                                                                                      alt="E-mail"></a>
-        <a href="/assets/file/Curriculum_VITAE.pdf" :title="$t('main.pdfFormat')" target="_blank"><img
-          class=logo-image src="@/assets/ico/download.svg" alt="Download file"></a>
-      </div>
+<!--        <a href="mailto:zorger27@gmail.com" :title="$t('main.email')" target="_blank"><img class=logo-image-->
+<!--                                                                                                      src="@/assets/ico/mail.svg"-->
+<!--                                                                                                      alt="E-mail"></a>-->
+<!--        <a href="/assets/file/Curriculum_VITAE.pdf" :title="$t('main.pdfFormat')" target="_blank"><img-->
+<!--          class=logo-image src="@/assets/ico/download.svg" alt="Download file"></a>-->
+<!--      </div>-->
       <div class="info">
         <p>
           {{ $t('main.info.text01') }}<br>
